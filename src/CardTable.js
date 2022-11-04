186b43316627318a6ad5bd8b8f606c651180b039
pm-moyanor <104218745+pm-moyanor@users.noreply.github.com>
2022-11-03 21:40:48 -0400

    removed React.strictMode

--- src/CardTable.js
+++ src/CardTable.js
@@ -3,25 +3,45 @@ import axios from "axios";
 
 const CardTable = () => {
   const [card, setCard] = useState("");
+let newActivity;
 
   useEffect(() => {
-    async function fetchData() {
-      let deck = await axios.get('https://www.deckofcardsapi.com/api/deck/new/')
-      let oneCard = await axios.get(
-        "https://www.deckofcardsapi.com/api/deck/ibft3ikg7kuj/draw/?count=1"
-      );
-      //const tryMe = oneCard.data.cards[0].image
-      return oneCard;
-    }
-    setCard(fetchData());
+    
+    
+     axios.get(
+        "https://www.boredapi.com/api/activity/"
+      ).then(res => console.log(res.data.activity));
+      //console.log(newActivity.data.activity);
+    setCard(newActivity.data.activity);
+    
+    //getActivity()
+
+   
   }, []);
-  
+
+  // async function getPlanet (){
+  //   return await axios.get("https://swapi.co/api/planets/1/")
+  // }
+  // getPlanet()
+
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     let deck = await axios.get('https://www.deckofcardsapi.com/api/deck/new/')
+  //     let oneCard = await axios.get(
+  //       "https://www.deckofcardsapi.com/api/deck/ibft3ikg7kuj/draw/?count=1"
+  //     );
+  //     //const tryMe = oneCard.data.cards[0].image
+  //     return oneCard;
+  //   }
+  //   setCard(fetchData());
+  // }, []);
 
   return (
     <div>
       <div></div>
-      <img src={card}></img>
-      <button>gimme a card</button>
+
+      <h3>{card ? card : "loading..."}</h3>
+      <button  >gimme a card</button>
     </div>
   );
 };
