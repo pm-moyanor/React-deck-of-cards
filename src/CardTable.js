90d1023a1734d23a3eabd3691aa794fda2cf99c5
pm-moyanor <104218745+pm-moyanor@users.noreply.github.com>
2022-11-07 09:37:20 -0500

    added basic CSS

--- src/CardTable.js
+++ src/CardTable.js
@@ -3,49 +3,35 @@ import axios from "axios";
 import Card from "./card";
 
 const CardTable = () => {
-  const [deck, setDeck] = useState("deck");
   const [card, setCard] = useState("card");
 
+
   useEffect(() => {
-    // axios
-    //   .get("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-    //   .then((res) => res = res.data.deck_id
-      
-    //   )
-    //   .then(
-    //     axios
-    //       .get(`https://www.deckofcardsapi.com/api/deck/${deck}/draw/?count=1`)
-    //       .then((res) => console.log(res))
-    //   );
-
-
-    axios.get("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-  .then((response) => {
-    return axios.get(`https://www.deckofcardsapi.com/api/deck/${response.data.deck_id}/draw/?count=1`); // using response.data
-  })
-  .then((response) => {
- 
-    setCard(response.data.cards[0])
-
-    
-  });
-
-
+    axios
+      .get("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+      .then((response) => {
+        return axios.get(
+          `https://www.deckofcardsapi.com/api/deck/${response.data.deck_id}/draw/?count=1`
+        );
+      })
+      .then((response) => {
+        setCard(response.data.cards[0]);
+      });
   }, []);
 
-const cardCode = card.code
-const cardImg = card.image
-  
+  const cardCode = card.code;
+  const cardImg = card.image;
 
- console.log(card)
+  const handleClick=()=>{
+    console.log("click")
+  }
+
 
   return (
-    <div className="game-container" >
-      <button className="btn">hit me!</button>
-     
-      <img classname="card-img" src={cardImg}></img>
-    
-     
+    <div className="game-container">
+      <button className="btn" onClick={handleClick}>hit me!</button>
+
+      <img className="card-img" src={cardImg}></img>
     </div>
   );
 };
